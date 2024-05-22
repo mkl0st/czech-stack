811b37160c843d362e4b90e6ae1e3d1f6aea8841
mkl0st <mklost@proton.me>
2024-05-22 14:30:44 +0200

    Adding and setting up Next

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Home | Czech Stack',
+  description: 'Czech Stack',
+};
+
+export default function Home() {
+  return (
+    <h1>Home</h1>
+  );
+}
